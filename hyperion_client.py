52211e6fbe6fa4e44808acb2350bb1c438009464
JFtechOfficial <36900161+JFtechOfficial@users.noreply.github.com>
2018-08-06 22:26:12 +0200

    Update hyperion_client.py

--- hyperion_client.py
+++ hyperion_client.py
@@ -1,4 +1,6 @@
+import json
 import socket
+import time
 
 s = None
 connected = False
@@ -29,69 +31,72 @@ def open_connection(host, port, timeout=10):
 def close_connection():
     if connected:
         try:
-            s.send('{"command":"clearall"}\n')
+            # s.send('{"command":"clearall"}\n')
             s.close()
         except socket.error, exc:
             print "Could not close socket connection\nMessage: ", exc
 
 
-def recv_timeout(the_socket,timeout=2):
-    #make socket non blocking
+def recv_timeout(the_socket, timeout=2):
+    # make socket non blocking
     the_socket.setblocking(0)
-     
-    #total data partwise in an array
-    total_data=[];
-    data='';
-     
-    #beginning time
-    begin=time.time()
+
+    # total data partwise in an array
+    total_data = []
+    data = ''
+
+    # beginning time
+    begin = time.time()
     while 1:
-        #if you got some data, then break after timeout
-        if total_data and time.time()-begin > timeout:
+        # if you got some data, then break after timeout
+        if total_data and time.time() - begin > timeout:
             break
-         
-        #if you got no data at all, wait a little longer, twice the timeout
-        elif time.time()-begin > timeout*2:
+
+        # if you got no data at all, wait a little longer, twice the timeout
+        elif time.time() - begin > timeout * 2:
             break
-         
-        #recv something
+
+        # recv something
         try:
             data = the_socket.recv(8192)
             if data:
                 total_data.append(data)
-                #change the beginning time for measurement
-                begin=time.time()
+                # change the beginning time for measurement
+                begin = time.time()
             else:
-                #sleep for sometime to indicate a gap
+                # sleep for sometime to indicate a gap
                 time.sleep(0.1)
         except:
             pass
-     
-    #join all parts to make final string
+
+    # join all parts to make final string
     return ''.join(total_data)
 
 
 def set_color(red, green, blue, priority=100, duration=0):
-        """
+    """
     Send the led data in a message format the hyperion json server understands
     :param led_data: bytearray of the led data (r,g,b) * hyperion.ledcount
     """
     if not connected:
         return
     # create a message to send
-    message = '{"color":[' + red + ',' + green + ',' + blue + '],'
-    message += '"command":"color","' + priority + '":100,"duration"' + duration + "}\n'
+    message = '{"color":[' + str(red) + ',' + str(green) + ',' + str(blue) + '],'
+    message += '"command":"color","' + str(priority)
+    message += '":100,"duration"' + str(duration) + '"}\n'
+
     try:
         s.send(message)
     except socket.error, exc:
         print "Error while sending the led data\nMessage: ", exc
-        
 
-def set_effect(effectName):#, effectArgs=None, priority=100, duration=0):
+
+def set_effect(effectName):  # , effectArgs=None, priority=100, duration=0):
     if not connected:
         return
     # create a message to send
-    message = '{"name":' + effectName + ',"command":"effect","priority":100}\n'
+    message = '{"command":"effect","effect":{"name":"Snake"},"priority":100}\n'
+    print message
     try:
         s.send(message)
     except socket.error, exc:
@@ -113,7 +118,7 @@ def clear_all():
     if not connected:
         return
     # create a message to send
-    message = '{"command":"clearall","priority":100}\n'
+    message = '{"command":"clearall"}\n'
     try:
         s.send(message)
     except socket.error, exc:
@@ -135,13 +140,13 @@ def get_serverinfo():
     except socket.error, exc:
         print "Error while reciving the data\nMessage: ", exc
     return resp
-    
-#def set_image()
-#def transform()
-#def correction()
-#def temperature()
-#def adjustment()
-        
+
+# def set_image()
+# def transform()
+# def correction()
+# def temperature()
+# def adjustment()
+
 
 def send_led_data(led_data):
     """
@@ -164,3 +169,16 @@ def send_led_data(led_data):
         s.send(message)
     except socket.error, exc:
         print "Error while sending the led data\nMessage: ", exc
+
+
+open_connection('192.168.1.26', 19444)
+set_effect("Snake")
+# clear_all()
+time.sleep(2)
+info = get_serverinfo()
+ppinfo = info.split('{"success":true}')
+print 'pretty server info:\n'
+parsed = json.loads(ppinfo[1])
+print json.dumps(parsed, indent=4, sort_keys=True)
+
+close_connection()
